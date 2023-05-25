ae27d6e39a6c2d4325d3d99efe35b33b666fd3c3
Abayomi Aluko <franicons@gmail.com>
2023-05-26 00:28:50 +0100

    test deploy

--- src/components/BannerComp.vue
+++ src/components/BannerComp.vue
@@ -2,28 +2,26 @@
 
 <template>
   <header>
-      <div class=" lg:py-52 md:py-48 py-36 max-w-7xl flex flex-col">
-        <div class="my-lg">
-          <h1 class="xl:text-6xl lg:text-6xl md:text-5xl sm:text-5xl text-4xl font-bold
-           bg-gradient-to-r from-cyan-100 via-zinc-100 to-purple-100 inline-block text-transparent bg-clip-text">
-            Hi, I’m Abayomi &lt;/&gt; The server warlock who controls dynamics in the making of web-app.
+      <div class="  max-w-6xl flex flex-col justify-center">
+        <div class="">
+          <div class="h-36 border-l my-4 bg-gradient-to-r border-zinc-600"></div>
+          <div class="pb-4 text-zinc-200 font-semibold">Hello, i'm Abayomi Aluko</div>
+          <h1 class="xl:text-8xl lg:text-7xl md:text-7xl sm:text-6xl text-5xl font-bold text-start
+           bg-gradient-to-r from-cyan-200 via-zinc-100 to-purple-100 inline-block text-transparent bg-clip-text  transition-all">
+<!--             &lt;/&gt; -->
+            The server warlock who controls  dynamics in the making of web-app.
           </h1>
           <h6 class="py-5 max-w-3xl sm:text-lg text-sm leading-6 mt-5
-          bg-gradient-to-r from-cyan-100 via-zinc-100 to-purple-100 inline-block text-transparent bg-clip-text
-          ">Having a good looking website is awesome, but making the web experience not to be static,
-            fast, secured & seamless is impeccable, giving web apps the least privilege to carry out it’s functionalities dynamically.</h6>
-          <div class="bg-zinc-800 text-white
-              px-2 py-3 w-52 text-center cursor-pointer hover:bg-zinc-700 transition-colors
-              ease-in-out shadow-lg flex justify-center items-center">
-            <div class="pr-2">About Me</div> 
-            <div class=""><img src="@/assets/images/forword.png" alt=""></div>
-          </div>
-          <div class="flex gap-8 py-8">
-            <img class="h-5 " src="@/assets/images/github.png" alt="">
+          bg-gradient-to-r from-purple-100 via-zinc-50 to-cyan-100 inline-block text-transparent bg-clip-text
+          "> Using my skill-set to help companies build clients & server side applications.  I'm a software engineer who operates in the dynamic making of applications that
+            lives on the internet and vocationally design responsive web applications. </h6>
+          <div class="flex gap-8 py-4">
+            <a href="" class=""><img class="h-5 " src="@/assets/images/github.png" alt=""></a>
             <img class="h-5" src="@/assets/images/linkedin.png" alt="">
             <img class="h-5" src="@/assets/images/telegram.png" alt="">
             <img class="h-5" src="@/assets/images/twitter.png" alt="">
           </div>
+          <div class="h-36 border-l mt-6 border-zinc-600"></div>
         </div>
       </div>
   </header>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import BannerComp from "@/components/BannerComp.vue";
+import ProjectsComp from "@/components/ProjectsComp.vue"
 </script>
 
 <template>
   <BannerComp/>
+  <ProjectsComp/>
 </template>
