0ba608496477845a7d5698c52af1acc5c682b386
Abayomi Aluko <franicons@gmail.com>
2023-05-31 07:36:34 +0100

    test-deploy

--- src/components/AboutComp.vue
+++ src/components/AboutComp.vue
@@ -105,23 +105,23 @@
             </div>
           </div>
           <div class="">
-            <div class="max-w-3xl mx-auto rounded-xl bg-gradient-to-tr via-zinc-900 to-zinc-900 mt-11 p-10 relative overflow-hidden">
+            <div class="max-w-3xl mx-auto rounded-xl bg-gradient-to-tr via-zinc-900 to-zinc-900 mt-11 xl:p-10 p-4 relative overflow-hidden">
               <h3 class="text-xl font-semibold mb-3">Experience</h3>
-              <p class="text-md font-normal text-zinc-300 xl:max-w-md max-w-full">
+              <p class="xl:text-md text-[14px] font-normal text-zinc-300 xl:max-w-md max-w-full">
                 Fast-forward to today I have had the opportunities to work remotely with diverse Company | Brand of different fields:
               </p>
               <div class="grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 grid-cols-1 mt-5 gap-3 backdrop-blur-2xl backdrop-filter">
                 <div class="bg-zinc-800 py-2 px-6 rounded-1xl rounded backdrop-blur-2xl backdrop-filter">
                   <h2 class="text-md font-semibold">Paychant</h2>
-                  <p class="">Software Engineer</p>
+                  <p class="text-[13px]">Software Engineer</p>
                 </div>
                 <div class="bg-zinc-800 py-2 px-6 rounded-1xl  rounded">
                   <h2 class="text-md font-semibold">Paramount Student</h2>
-                  <p class="">Lead Frontend Engineer</p>
+                  <p class="text-[13px]">Lead Frontend Engineer</p>
                 </div>
                 <div class="bg-zinc-800 py-2 px-6 rounded-1xl  rounded">
                   <h2 class="text-md font-semibold">AMD Dubai</h2>
-                  <p class="">Product Designer</p>
+                  <p class="text-[13px]">Product Designer</p>
                 </div>
               </div>
             </div>
