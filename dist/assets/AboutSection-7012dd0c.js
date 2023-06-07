36535cdebd894984754218068652804b1f1da63a
Abayomi Aluko <franicons@gmail.com>
2023-06-07 10:44:03 +0100

    refactoring

--- dist/assets/AboutSection-7012dd0c.js
+++
@@ -1,1 +0,0 @@
-import{a as m}from"./AboutSection.vue_vue_type_script_setup_true_lang-03357f1d.js";import"./index-76c8a436.js";export{m as default};
---
+++ dist/assets/AboutSection-c21bd627.js
@@ -0,0 +1,1 @@
+import{a as m}from"./AboutSection.vue_vue_type_script_setup_true_lang-6a595e2e.js";import"./index-36638d82.js";export{m as default};
